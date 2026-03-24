3d0513c0232120f203d2a759e7a34b888c02a5d8
dedalivfr <sp4066@srmist.edu.in>
2026-03-25 00:05:15 +0530

    UC 1 - Application Entry & Welcome Message

---
+++ App/src/BookMyStayApp.java
@@ -0,0 +1,12 @@
+public class BookMyStayApp {
+    public class UseCase1HotelBookingApp {
+
+       public static void main(String[] args) {
+          System.out.println("======================================");
+          System.out.println("Welcome to the Hotel Booking System!");
+          System.out.println("System has started successfully.");
+          System.out.println("======================================");
+       }
+    }
+    
+}
