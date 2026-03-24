97f1abc1ed3c605530312418593644b357bdf732
dedalivfr <sp4066@srmist.edu.in>
2026-03-25 00:16:21 +0530

    UC2 - Basic Room Types & Static Availability

--- App/src/BookMyStayApp.java
+++ App/src/BookMyStayApp.java
@@ -1,12 +1,21 @@
 public class BookMyStayApp {
-    public class UseCase1HotelBookingApp {
+    public abstract class Room {
 
-       public static void main(String[] args) {
-          System.out.println("======================================");
-          System.out.println("Welcome to the Hotel Booking System!");
-          System.out.println("System has started successfully.");
-          System.out.println("======================================");
-       }
-    }
-    
+      protected int numberOfBeds;
+      protected int squareFeet;
+      protected double pricePerNight;
+
+      public Room(int numberOfBeds, int squareFeet, double pricePerNight) {
+        this.numberOfBeds = numberOfBeds;
+        this.squareFeet = squareFeet;
+        this.pricePerNight = pricePerNight;
+      }
+
+      public void displayRoomDetails() {
+        System.out.println("Room Details:");
+        System.out.println("Number of Beds: " + numberOfBeds);
+        System.out.println("Room Size (sq ft): " + squareFeet);
+        System.out.println("Price per Night: $" + pricePerNight);
+      }
+   }
 }
